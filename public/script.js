fc299c2dc0a15304ef240c3bf5940df254a33e8e
buddha <buddhatamang1000@gmail.com>
2021-11-18 18:26:29 +0545

    first commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,62 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{
+				title: "product 1",
+				id: 1,
+				price: 9.99,
+			},
+			{
+				title: "product 2",
+				id: 2,
+				price: 9.99,
+			},
+			{
+				title: "product 3",
+				id: 3,
+				price: 9.99,
+			},
+		],
+		cart: [],
+	},
+	methods: {
+		addToCart: function (product) {
+			this.total += product.price;
+			let found = false;
+			console.log(this.cart);
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					tital: product.tital,
+					price: product.price,
+					qty: 1,
+				});
+			}
+		},
+		addItm: function (item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		remItm: function (item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i, 1);
+			}
+		},
+	},
+	filters: {
+		currency: function (price) {
+			return "$".concat(price.toFixed(2));
+		},
+	},
+});
